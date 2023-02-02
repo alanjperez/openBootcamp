085270304134884ed5b3eb8178ecc7ed446dc6ad
Alan Perez <alanjperez@gmail.com>
2023-02-01 18:27:29 -0600

    primera tarea

---
+++ ejercicio_2.js
@@ -0,0 +1,18 @@
+let miNombre = "Alan Perez"
+console.log(miNombre)
+
+let miEdad = 36;
+console.log(miEdad)
+
+let soyDesarrollador = true; 
+console.log(soyDesarrollador)
+
+const fecha_nacimiento = new Date(1986 ,3, 28);
+console.log(fecha_nacimiento)
+
+const libro_favorito = {
+    nombre: "psicologia de lo obscuro",
+    autor: "Steven Turner",
+    fecha: "2000"
+}
+console.log(libro_favorito.autor)
